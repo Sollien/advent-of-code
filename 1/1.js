73eab488ac9c116a11dcbb83eba44c0535ccc39a
Martin <51482460+Sollien@users.noreply.github.com>
2023-12-02 17:45:49 +0100

    Puzzle one code solution

--- 1/1.js
+++ 1/1.js
@@ -1,4 +1,27 @@
 const fs = require('fs')
-const input = fs.readFileSync("./input.txt", "utf-8")
+const inputList = fs.readFileSync("./input.txt", "utf-8").split("\n")
+const sumList = []
 
-console.log(input)
+for (let i = 0; i < inputList.length; i++) {
+	const numbersInString = inputList[i].match(/\d/g)
+
+	if (numbersInString) {
+		const stringToInt = numbersInString.map(match => parseInt(match, 10))
+
+		if (stringToInt.length === 1) {
+			const concatenatedNumbers = parseInt(stringToInt[0].toString() + stringToInt[0].toString(), 10)
+
+			sumList.push(concatenatedNumbers)
+		} else {
+			const firstNumber = stringToInt[0]
+			const lastNumber = stringToInt[stringToInt.length - 1]
+			const concatenatedNumbers = parseInt(firstNumber.toString() + lastNumber.toString(), 10)
+
+			sumList.push(concatenatedNumbers)
+		}
+	}
+}
+
+const sum = sumList.reduce((accumulator, currentNumber) => accumulator + currentNumber, 0)
+
+console.log(sum)
