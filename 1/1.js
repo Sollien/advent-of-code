7a924ff944c7562e1ebb89b8a1001e7703c63c40
Martin <51482460+Sollien@users.noreply.github.com>
2023-12-04 12:31:34 +0100

    AoC 1 part 2 solution

--- 1/1.js
+++ 1/1.js
@@ -1,9 +1,22 @@
 const fs = require('fs')
-const inputList = fs.readFileSync("./input.txt", "utf-8").split("\n")
+let input = fs.readFileSync("./input.txt", "utf-8").split("\n")
+
+input = input.map(line => line
+	.replace(/one/g, "o1e")
+	.replace(/two/g, "t2o")
+	.replace(/three/g, "t3e")
+	.replace(/four/g, "f4")
+	.replace(/five/g, "f5e")
+	.replace(/six/g, "s6")
+	.replace(/seven/g, "s7n")
+	.replace(/eight/g, "e8t")
+	.replace(/nine/g, "n9e")
+)
+
 const sumList = []
 
-for (let i = 0; i < inputList.length; i++) {
-	const numbersInString = inputList[i].match(/\d/g)
+for (let i = 0; i < input.length; i++) {
+	const numbersInString = input[i].match(/\d/g)
 
 	if (numbersInString) {
 		const stringToInt = numbersInString.map(match => parseInt(match, 10))
