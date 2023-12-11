e737c79fad92a323aa1f46b42e946aa7d6450e66
Martin <51482460+Sollien@users.noreply.github.com>
2023-12-11 09:17:18 +0100

    AoC C# WiP

---
+++ 2023/1/C#/1.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] input = File.ReadAllLines("./../input.txt");
+
+foreach (string line in input) {
+	input = line
+		.Replace("one", "o1e")
+		.Replace("two", "t2o")
+		.Replace("three", "t3e")
+		.Replace("four", "f4")
+		.Replace("five", "f5e")
+		.Replace("six", "s6")
+		.Replace("seven", "s7n")
+		.Replace("eight", "e8t")
+		.Replace("nine", "n9");
+}
+
+Console.WriteLine(input);
