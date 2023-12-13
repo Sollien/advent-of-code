1ad746d78ef4c9ba54b5440b4e15210c429f94e1
Martin <51482460+Sollien@users.noreply.github.com>
2023-12-13 15:24:50 +0100

    AoC day 1, C# wip

--- 2023/1/C#/1.cs
+++ 2023/1/C#/1.cs
@@ -1,19 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
 string[] input = File.ReadAllLines("./../../../../input.txt");
+int[] sumList;
 
-foreach (string line in input) {
-    string replaceWordedNumbers = line.Replace("one", "o1e")
-		.Replace("two", "t2o")
-		.Replace("three", "t3e")
-		.Replace("four", "f4")
-		.Replace("five", "f5e")
-		.Replace("six", "s6")
-		.Replace("seven", "s7n")
-		.Replace("eight", "e8t")
-		.Replace("nine", "n9");
-    
-	Console.WriteLine(replaceWordedNumbers);
+input = input.Select(s => s
+	.Replace("one", "o1e")
+    .Replace("two", "t2o")
+    .Replace("three", "t3e")
+    .Replace("four", "f4")
+    .Replace("five", "f5e")
+    .Replace("six", "s6")
+    .Replace("seven", "s7n")
+    .Replace("eight", "e8t")
+    .Replace("nine", "n9")).ToArray();
+
+for (int i = 0; i < input.Length; i++) {
+    MatchCollection numbersInString = new Regex(@"\b[0-9]\w*\b").Matches(input[i]);
+
+    foreach (var number in numbersInString)
+    {
+        Console.WriteLine(number);
+    }
 }
+
 
 Console.ReadLine();
